333672eb33947bef7ffe0a20d710f9a6487aa77e
KevinP <kevin.peron22@gmail.com>
2017-06-26 09:44:54 +0200

    Big maj, adding few files with some tests

---
+++ js/form-init.js
@@ -0,0 +1,35 @@
+(function($){
+  $(function(){
+
+
+    $('select').material_select();
+	
+	
+	
+	/*$('.chips').material_chip();
+	  $('.chips-initial').material_chip({
+		data: [{
+		  tag: 'Apple',
+		}, {
+		  tag: 'Microsoft',
+		}, {
+		  tag: 'Google',
+		}],
+	  });
+	  $('.chips-placeholder').material_chip({
+		placeholder: 'Enter a tag',
+		secondaryPlaceholder: '+Tag',
+	  });
+        
+	  $('.datepicker').pickadate({
+		selectMonths: true, // Creates a dropdown to control month
+		selectYears: 15 // Creates a dropdown of 15 years to control year
+	  });
+	  $('.timepicker').pickatime({
+		  formatSubmit: 'H:i',
+		   clear: 'Effacer'
+	  });*/
+	  
+	  
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
